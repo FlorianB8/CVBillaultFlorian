6c7649da09b7e4e7c37f7b361bb61dbcfc1b066a
FlorianB8 <billaultflo@gmail.com>
2023-06-19 15:45:17 +0200

    Amélioration

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,6 +3,10 @@ var speed = 100; /* The speed/duration of the effect in milliseconds */
 let lastname = 'BILLAULT';
 let cptFirstname = 0;
 let cptLastname=0;
+let videoTrip = document.getElementById('videoTrip');
+let videoCoding = document.getElementById('videoCoding');
+let videoReading = document.getElementById('videoReading');
+let videoGaming = document.getElementById('videoGaming');
 
 function firstnameWriter() {
     if (cptFirstname < firstname.length) {
@@ -12,16 +16,50 @@ function firstnameWriter() {
     }
 }
 function lastnameWriter() {
+    
     if (cptLastname < lastname.length) {
         document.getElementById("lastname").innerHTML += lastname.charAt(cptLastname);
         cptLastname++;
         setTimeout(lastnameWriter, speed);
     }
+    
 }
 window.addEventListener('DOMContentLoaded', firstnameWriter());
 
-window.addEventListener('DOMContentLoaded',setTimeout(() => {
-    lastnameWriter();
-}, 1000))
+window.addEventListener('DOMContentLoaded', lastnameWriter());
 
 
+videoTrip.addEventListener('mouseover', () => {
+    videoTrip.play();
+
+})
+
+videoTrip.addEventListener('mouseout', () => {
+    videoTrip.pause();
+})
+
+videoCoding.addEventListener('mouseover', () => {
+    videoCoding.play();
+
+})
+
+videoCoding.addEventListener('mouseout', () => {
+    videoCoding.pause();
+})
+
+videoReading.addEventListener('mouseover', () => {
+    videoReading.play();
+
+})
+
+videoReading.addEventListener('mouseout', () => {
+    videoReading.pause();
+})
+videoGaming.addEventListener('mouseover', () => {
+    videoGaming.play();
+
+})
+
+videoGaming.addEventListener('mouseout', () => {
+    videoGaming.pause();
+})
